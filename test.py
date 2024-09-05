1737ed7befe3630845770180cd697a8ac0e81033
voxwos <voxwos@yandex.com>
2024-09-05 15:37:13 +0600

    1

---
+++ test.py
@@ -0,0 +1,32 @@
+class Error(Exception):
+    pass
+
+def main(input_: str):
+    try:
+        a = input_.split()
+        a[0], a[2] = int(a[0]), int(a[2])
+        if len(a) != 3:
+            raise Error("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)")
+        if a[0] in range(1, 11) and a[2] in range(1, 11):
+            if a[1] == "+":
+                print(a[0] + a[2])
+            elif a[1] == "-":
+                print(a[0] - a[2])
+            elif a[1] == "*":
+                print(a[0] * a[2])
+            elif a[1] == "/":
+                print(a[0] // a[2])
+            else:
+                raise Error("Неверная операция")
+        else:
+            raise Error("Неверный формат данных")
+    except ValueError:
+        print("Неверный формат данных")
+    except IndexError:
+        print("Неверный формат данных")
+    except Error as e:
+        print(e)
+
+while True:
+    user_input = input()
+    main(user_input)
